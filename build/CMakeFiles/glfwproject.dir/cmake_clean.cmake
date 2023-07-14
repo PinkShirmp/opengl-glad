eabdd64b6658c1d4ec1125af43919d788e9af572
NGUYEN MINH TUE <nminhtue094@gmail.com>
2023-07-14 19:23:49 +0700

    linked opengl

---
+++ build/CMakeFiles/glfwproject.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/glfwproject.dir/main.cpp.obj"
+  "CMakeFiles/glfwproject.dir/main.cpp.obj.d"
+  "glfwproject.exe"
+  "glfwproject.exe.manifest"
+  "glfwproject.pdb"
+  "libglfwproject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/glfwproject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
